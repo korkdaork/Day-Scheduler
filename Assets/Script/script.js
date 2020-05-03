2985490e6f314d60c58df43f4fbaf50a18b39e27
Yev Ro <yev.rokhin@gmail.com>
2020-05-03 12:06:51 -0700

    updated styles, format in html (replaced input w/ textarea, and finally used more efficient script that was explained by Justin.  The route I initially took was very inefficient and repetative - glad I instructions

--- Assets/Script/script.js
+++ Assets/Script/script.js
@@ -1,11 +1,88 @@
 $(document).ready(function () {
 
+    startIt();
+
+    //organize function start
+    function startIt() {
+        getCurrentDay();
+        timeCheck();
+        loadDescription()
+    }
+
+
+
+    //====================================================BUTTONS=====================================================================
+
+    //Save button function - onclick - to save data in local storage
+    $(".saveBtn").on("click", function () {
+
+        //identify data-time number from each description class
+        const timeId = $(this).siblings(".description").attr("data-time");
+
+        //indentify user input in each description based on time id
+        const userInput = $(this).siblings(".description").val();
+
+        //save information in local storage
+        localStorage.setItem(timeId, userInput);
+    })
+
+    //==========================================================FUNCTIONS==================================================================
+
+    //Retrieve the description/task text from local storage
+    function loadDescription() {
+
+        //get array of description elements
+        const descriptions = $(".description")
+
+        //for loop w/ each element
+        $(descriptions).each(function (i, element) {
+
+            //retreives data from local storage based on time id from data-time
+            const timeId = $(element).attr("data-time");
+
+            //get description from each unique element tag
+            const description = localStorage.getItem(timeId);
+
+            //this sets the text back in each unique element (i.e. in each specific hour)
+            $(element).text(description);
+        })
+    }
+
+    //get current date
     function getCurrentDay() {
         const currentDay = moment().format("dddd, MMMM Do");
         $("#currentDay").text(currentDay);
 
-    }
+    };
 
-    getCurrentDay();
+    //get current hour
+    function timeCheck() {
+        const currentHour = moment().hours();
+
+        //target description tag
+        const descriptions = $(".description");
+
+        //loop thorugh hours
+        $(descriptions).each(function (index, element) {
+
+            //current time that is stored on the description block
+            const currentTime = parseInt($(element).attr("data-time"));
+
+            //if currentTime(time on our calendar) is less than current hour - then it is in the past... current time(calendar) = current hour -> present | and finally everything else = future
+            if (currentTime < currentHour) {
+                $(element).addClass("past");
+            }
+            else if (currentTime == currentHour) {
+                $(element).addClass("present");
+            }
+            else {
+                $(element).addClass("future");
+            }
+        });
+
+
+    };
+
+
 
 })
